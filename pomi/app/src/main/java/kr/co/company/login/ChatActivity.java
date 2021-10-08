03884837f996be94717786ecd9c3ab28b7b360b4
limsubinn <mymyx02@naver.com>
2021-10-08 20:36:01 +0900

    211008

--- pomi/app/src/main/java/kr/co/company/login/ChatActivity.java
+++ pomi/app/src/main/java/kr/co/company/login/ChatActivity.java
@@ -1,6 +1,9 @@
 package kr.co.company.login;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Message;
@@ -10,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -40,7 +44,14 @@ import java.util.UUID;
 import kr.co.company.login.adapters.ChatAdapter;
 import kr.co.company.login.helpers.SendMessageInBg;
 import kr.co.company.login.interfaces.BotReply;
+import retrofit.DeleteData;
+import retrofit.DeleteResponse;
 import retrofit.MessageData;
+import retrofit.RetrofitClient;
+import retrofit.ServiceApi;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ChatActivity extends AppCompatActivity implements BotReply {
     Toolbar toolbar;
@@ -50,6 +61,9 @@ public class ChatActivity extends AppCompatActivity implements BotReply {
     EditText editMessage;
     ImageButton btnSend;
     Button backButton;
+    ImageButton helpbtn;
+    TextView tv_name;
+    ServiceApi service;
 
     DrawerLayout drawerLayout;
     View drawerView;
@@ -65,20 +79,18 @@ public class ChatActivity extends AppCompatActivity implements BotReply {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
         chatView = findViewById(R.id.chatView);
         editMessage = findViewById(R.id.editMessage);
         btnSend = findViewById(R.id.btnSend);
         backButton = findViewById(R.id.backButton);
+        helpbtn = findViewById(R.id.helpButton);
+
 
         chatAdapter = new ChatAdapter(messageList, this);
         chatView.setAdapter(chatAdapter);
 
         messageList.add(new MessageData("안녕하세요. 저는 인공지능 챗봇입니다.\n 신청할 민원을 내용을 직접 입력해주세요.\n( ex. 신고, 조회 )", true));
-
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("userName");
-        String id = intent.getStringExtra("userId");
-
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,6 +121,137 @@ public class ChatActivity extends AppCompatActivity implements BotReply {
             public void onClick(View view) {
                 drawerLayout.openDrawer(drawerView);
             }
+        });
+
+        helpbtn = findViewById(R.id.helpButton);
+        helpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ChatActivity.this);
+                builder.setTitle("민원 입력 Tip! \n")
+                        .setMessage("1. 조회 \n" +
+                                "  -코드 \n" +
+                                "  -현황 \n" +
+                                "ex) 코드 알려줘, 현황 알려줘 \n" +
+                                "2. 자주하는 질문(FAQ) \n" +
+                                "ex) 고지서가 안보여, 신고내역 보는 법 \n")
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i)
+                            {  }
+                        });
+
+                builder.show();
+            }
+        });
+
+        TextView call_portmis =findViewById(R.id.call_portmis);
+        call_portmis.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:18001172"));
+                startActivity(intent);
+            }
+        });
+
+        TextView mv_website = findViewById(R.id.mv_website);
+        mv_website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(intent);
+            }
+        });;
+
+
+        Intent intent = getIntent(); // 실려온 인텐트 정보를 담음
+        String name = intent.getStringExtra("userName");
+        String id = intent.getStringExtra("userId");
+
+        tv_name = findViewById(R.id.user_name);
+        tv_name.setText(name);
+
+        service = RetrofitClient.getClient().create(ServiceApi.class);
+
+        Button deleteMemberButton = (Button) findViewById(R.id.deleteMember);
+        deleteMemberButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String input_id = id;
+                EditText editText = new EditText(ChatActivity.this);
+                androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(ChatActivity.this);
+                builder.setTitle("정말로 탈퇴하시겠습니까? \n" +
+                        "비밀번호를 입력해주세요.")
+                        .setView(editText);
+
+
+
+                builder.setPositiveButton("탈퇴", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        String input_pw = editText.getText().toString();
+
+                        //데이터베이스의 passwd 비교
+                        service.userDelete(new DeleteData(input_pw, input_id )).enqueue(new Callback<DeleteResponse>() {
+
+                            @Override
+                            public void onResponse(Call<DeleteResponse> call, Response<DeleteResponse> response) {
+                                DeleteResponse result = response.body();
+
+                                if (result.getCode() == 200) {
+                                    Toast.makeText(ChatActivity.this, result.getMessage(), Toast.LENGTH_SHORT).show();
+                                    Intent i = new Intent(ChatActivity.this, MainActivity.class);
+                                    i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(i);
+                                    //Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    //startActivity(intent);
+                                } else {
+                                    Toast.makeText(ChatActivity.this, result.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<DeleteResponse> call, Throwable t) {
+                                Toast.makeText(ChatActivity.this, "탈퇴 에러", Toast.LENGTH_SHORT).show();
+                                Log.e("탈퇴 에러", t.getMessage());
+
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+
+                            }
+                        });
+
+
+
+                    }
+                });
+
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "회원탈퇴를 취소하였습니다", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+                builder.show();
+
+            }
+
+        });
+        Button logoutButton = (Button) findViewById(R.id.logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ChatActivity.this, "로그아웃 되었습니다", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(ChatActivity.this, MainActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(i);
+            }
+
         });
     }
 
@@ -171,8 +314,5 @@ public class ChatActivity extends AppCompatActivity implements BotReply {
             Toast.makeText(this, "failed to connect!", Toast.LENGTH_SHORT).show();
         }
     }
-
-
-
 
 }
